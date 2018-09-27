5421ae3908cb3288081074c042f4301408abaa77
Anthony Lee <don.anthony.lee@gmail.com>
2018-09-27 20:03:17 +0800

    插件化面板设备

--- src/include/lbk/add-ons/LBPanelDevice.h
+++ src/include/lbk/add-ons/LBPanelDevice.h
@@ -28,10 +28,74 @@
  *
  * --------------------------------------------------------------------------*/
 
-#ifndef __LBK_ADD_ONS_PANEL_DEVICE_H__
-#define __LBK_ADD_ONS_PANEL_DEVICE_H__
+#ifndef __LBK_PANEL_DEVICE_H__
+#define __LBK_PANEL_DEVICE_H__
 
-// TODO
+#include <lbk/LBKConfig.h>
+#include <lbk/LBApplication.h>
 
-#endif /* __LBK_ADD_ONS_PANEL_DEVICE_H__ */
+#ifdef __cplusplus /* Just for C++ */
+
+// NOTE:
+// 	Addon must have C function like below and all the null virtual functions
+// 	of class must be implemented.
+// 		extern "C" LBPanelDevice* instantiate_panel_device();
+
+class LBPanelDevice : public BLocker {
+public:
+	LBPanelDevice(const BList *cfg);
+	virtual ~LBPanelDevcie();
+
+	virtual status_t	InitCheck() = 0;
+
+	/* screen */
+	virtual uint16		ScreenWidth() = 0;
+	virtual uint16		ScreenHeight() = 0;
+#ifdef LBK_ENABLE_MORE_FEATURES
+	virtual uint8		ScreenDepth() = 0;
+	virtual status_t	SetHighColor(rgb_color c) = 0;
+	virtual status_t	SetLowColor(rgb_color c) = 0;
+#endif
+	virtual status_t	FillRect(BRect rect,
+					 pattern p,
+					 bigtime_t &ts) = 0;
+	virtual status_t	DrawString(const char *str,
+					   BPoint pt,
+					   uint8 fontHeight,
+					   bigtime_t &ts) = 0;
+	virtual status_t	MeasureStringWidth(const char *str,
+						   uint8 fontHeight,
+						   uint16 &w,
+						   uint16 &h,
+						   bigtime_t &ts) = 0;
+	virtual status_t	GetPowerState(bool &state) = 0;
+	virtual status_t	SetPowerState(bool state) = 0;
+	virtual status_t	GetTimestamp(bigtime_t &ts) = 0;
+	virtual status_t	SetTimestamp(bigtime_t ts) = 0;
+	virtual status_t	DisableUpdate() = 0;
+	virtual status_t	EnableUpdate() = 0;
+#ifdef LBK_ENABLE_MORE_FEATURES
+	virtual status_t	MapBuffer(void **buf) = 0;
+	virtual status_t	UnmapBuffer(void *buf) = 0;
+#endif
+
+	/* keys */
+	virtual status_t	GetCountOfKeys(uint8 &count) = 0;
+#ifdef LBK_ENABLE_MORE_FEATURES
+	virtual status_t	GetOrientationOfKeys(orientation &o) = 0;
+	virtual status_t	GetSideOfKeys(bool &right_or_bottom) = 0;
+	virtual status_t	GetScreenOffsetOfKeys(uint16 &offset) = 0;
+#endif
+
+	void			SendMessageToApp(const BMessage *msg);
+
+private:
+	friend class LBApplication;
+
+	BMessenger fMsgr;
+};
+
+#endif /* __cplusplus */
+
+#endif /* __LBK_PANEL_DEVICE_H__ */
 
