d13ef5c996df3de18b1b4d723585821f93fed733
Anthony Lee <don.anthony.lee@gmail.com>
2018-10-04 01:08:58 +0800

    LBScopeHandler: 完善

--- src/include/lbk/LBScopeHandler.h
+++ src/include/lbk/LBScopeHandler.h
@@ -64,41 +64,58 @@ public:
 	virtual void		RollUp();
 	virtual void		RollDown();
 
-	// Empty functions BEGIN --- just for derived class
-	virtual void		PositionChanged(int32 pos, int32 old);
-	virtual void		OffsetChanged(int32 offset, int32 old);
-	virtual void		ScopeChanged();
-	// Empty functions END
-
 	void			ShowItem(LBScopeItem *item);
 	void			HideItem(LBScopeItem *item);
 	virtual void		ShowItem(int32 index);
 	virtual void		HideItem(int32 index);
 	bool			IsItemVisible(const LBScopeItem *item) const;
 
+	bool			IsExpanded() const;
 	virtual void		Expand();
 	virtual void		Collapse();
 
-	virtual void		Show();
-	virtual void		Hide();
-
 protected:
+	// Empty functions BEGIN --- just for derived class
+	virtual void		PositionChanged(int32 pos, int32 old);
+	virtual void		OffsetChanged(int32 offset, int32 old);
+	virtual void		ScopeChanged();
+	// Empty functions END
+
 	virtual int32		VisibleItemsCountMax() const;
 
 	int32			CountVisibleItems(int32 fromIndex, int32 n) const;
 	LBScopeItem*		PrevVisibleItem(int32 &index) const;
 	LBScopeItem*		NextVisibleItem(int32 &index) const;
-	LBScopeItem*		FirstVisibleItem(int32 &index, int32 fromOffset = 0) const;
+	LBScopeItem*		FirstVisibleItem(int32 &index, int32 fromIndex = 0) const;
 	LBScopeItem*		LastVisibleItem(int32 &index) const;
-	LBScopeItem*		FirstVisibleItemAtScope(int32 &index) const;
+
+	int32			CountVisibleItemsAtScope(int32 fromIndex, int32 n) const;
+	LBScopeItem*		FirstVisibleItemAtScope(int32 &index, int32 fromIndex = -1) const;
 	LBScopeItem*		LastVisibleItemAtScope(int32 &index) const;
+	LBScopeItem*		PrevVisibleItemAtScope(int32 &index) const;
+	LBScopeItem*		NextVisibleItemAtScope(int32 &index) const;
 
 private:
 	BList fItems;
 	int32 fOffset;
 	int32 fPos;
+	int32 fExpanded;
 
-	virtual int32		RejustOffsetAfterPositionChanged();
+	/*
+	 * Override following virtual functions to change default behavior.
+	 * 	ReadjustOffsetWhenPositionChanged(): return new offset.
+	 * 	ReadjustWhenItemShown(): return true if scope changed.
+	 * 	ReadjustWhenItemHidden(): return true if scope changed.
+	 * 	ReadjustWhenItemsSwapped(): return true if scope changed.
+	 */
+	virtual int32		ReadjustOffsetWhenPositionChanged() const;
+	virtual bool		ReadjustWhenItemShown(int32 item_index,
+						      int32 &new_offset, int32 &new_pos) const;
+	virtual bool		ReadjustWhenItemHidden(int32 item_index,
+						       int32 &new_offset, int32 &new_pos) const;
+	virtual bool		ReadjustWhenItemsSwapped(int32 itemA_index, int32 itemB_index,
+							 int32 last_index_at_scope_before,
+							 int32 &new_offset, int32 &new_pos) const;
 };
 
 #endif /* __cplusplus */
