de1e501ee7d789d8430d2be2833aff87b076dac5
Anthony Lee <don.anthony.lee@gmail.com>
2018-11-05 18:26:09 +0800

    autoconf相关: 修正 OpenWrt 编译环境相关

--- src/app/panel/test-panel.sh
+++ src/app/panel/test-panel.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-LIBRARY_PATH=${LIBRARY_PATH}:../../libs/lbk
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:../../libs/lbk
-exec ./LBPanel $*
+LIBDIR="../../libs/lbk"
+
+if [ -x "./lb_panel" ]; then
+	# for autoconf environment
+	PROG="./lb_panel"
+elif [ -x "./LBPanel" ]; then
+	PROG="./LBPanel"
+else
+	echo "Error: no executable file!"
+	exit 1
+fi
+
+LIBRARY_PATH="${LIBRARY_PATH}:${LIBDIR}" LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${LIBDIR}" $PROG $*
+
