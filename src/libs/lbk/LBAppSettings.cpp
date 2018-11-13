65004d5e69c8f03a26322434cabc2e398ada04af
Anthony Lee <don.anthony.lee@gmail.com>
2018-11-14 00:44:56 +0800

    LBKit: LBAppSettings 增加 GetItemAt()，同时调整为不忽略空行，方便设置文件回写

--- src/libs/lbk/LBAppSettings.cpp
+++ src/libs/lbk/LBAppSettings.cpp
@@ -58,14 +58,13 @@ bool
 LBAppSettings::AddItem(const char *item, int32 index)
 {
 	char *data = (item == NULL || *item == 0) ? NULL : strdup(item);
-	if(data == NULL) return false;
 
 	if(index < 0)
 		index = fSettings.CountItems();
 
 	if(fSettings.AddItem(data, index) == false)
 	{
-		free(data);
+		if(data != NULL) free(data);
 		return false;
 	}
 
@@ -100,31 +99,35 @@ LBAppSettings::AddItems(BFile *f, int32 index)
 	free(buf);
 
 	int32 offset = 0, found;
+	char *data;
+
 	while(offset < str.Length())
 	{
 		found = str.FindFirst("\n", offset);
 		if(found < offset)
 			found = str.Length();
 
+		data = NULL;
+
 		if(found > offset)
 		{
 #if 0
-			char *data = strndup(str.String() + offset, found - offset);
+			data = strndup(str.String() + offset, found - offset);
 #else
-			char *data = (char*)malloc(found - offset + 1);
-			if(data != NULL)
+			if((data = (char*)malloc(found - offset + 1)) != NULL)
 			{
 				memcpy(data, str.String() + offset, found - offset);
 				*(data + found - offset) = 0;
 			}
 #endif
-
-			if(data == NULL || cfg.AddItem(data) == false)
-			{
-				if(data != NULL) free(data);
-				goto failed;
-			}
 		}
+
+		if((found > offset && data == NULL) || cfg.AddItem(data) == false)
+		{
+			if(data != NULL) free(data);
+			goto failed;
+		}
+
 		offset = found + 1;
 	}
 
@@ -133,8 +136,8 @@ LBAppSettings::AddItems(BFile *f, int32 index)
 failed:
 	for(int32 k = 0; k < cfg.CountItems(); k++)
 	{
-		char *data = (char*)cfg.ItemAt(k);
-		free(data);
+		data = (char*)cfg.ItemAt(k);
+		if(data != NULL) free(data);
 	}
 
 	return false;
@@ -151,8 +154,8 @@ LBAppSettings::AddItems(const LBAppSettings &settings, int32 index)
 	BList cfg;
 	for(int32 k = 0; k < settings.CountItems(); k++)
 	{
-		char *data = strdup(settings.ItemAt(k));
-		if(data == NULL || cfg.AddItem(data) == false)
+		char *data = (settings.ItemAt(k) == NULL) ? NULL : strdup(settings.ItemAt(k));
+		if(cfg.AddItem(data) == false)
 		{
 			if(data != NULL) free(data);
 			goto failed;
@@ -165,7 +168,7 @@ failed:
 	for(int32 k = 0; k < cfg.CountItems(); k++)
 	{
 		char *data = (char*)cfg.ItemAt(k);
-		free(data);
+		if(data != NULL) free(data);
 	}
 
 	return false;
@@ -178,7 +181,7 @@ LBAppSettings::MakeEmpty()
 	for(int32 k = 0; k < fSettings.CountItems(); k++)
 	{
 		char *data = (char*)fSettings.ItemAt(k);
-		free(data);
+		if(data != NULL) free(data);
 	}
 	fSettings.MakeEmpty();
 }
@@ -211,6 +214,49 @@ const char*
 LBAppSettings::ItemAt(int32 index) const
 {
 	return (const char*)fSettings.ItemAt(index);
+}
+
+
+status_t
+LBAppSettings::GetItemAt(int32 index,
+			 BString *name,
+			 BString *value,
+			 BString *options) const
+{
+	if(index < 0 || index >= fSettings.CountItems()) return B_BAD_INDEX;
+	if(name == NULL) return B_BAD_VALUE;
+
+	do
+	{
+		BString item((char*)fSettings.ItemAt(index));
+		if(item.Length() == 0) break;
+		if(item.ByteAt(0) == '#' || item.FindFirst("//") == 0) break; // comment
+
+		int32 found = item.FindFirst("=");
+		if(found <= 0 || found == item.Length() - 1) break;
+
+		BString strName(item.String(), found);
+		BString strValue(item.String() + found + 1);
+
+		if(options != NULL)
+		{
+			found = strValue.FindFirst(",");
+			if(found == 0) break;
+			if(found > 0)
+			{
+				if(found < strValue.Length() - 1)
+					options->SetTo(strValue.String() + found + 1);
+				strValue.Truncate(found);
+			}
+		}
+
+		*name = strName;
+		if(value) *value = strValue;
+
+		return B_OK;
+	} while(false);
+
+	return B_ERROR;
 }
 
 
