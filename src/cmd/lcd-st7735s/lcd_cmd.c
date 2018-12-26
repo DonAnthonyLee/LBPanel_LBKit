9d82edc9357a3d0678c72209bdd7c0170040b3ca
Anthony Lee <don.anthony.lee@gmail.com>
2018-12-27 02:56:16 +0800

    cmd: 增加 LCD ST7735S 命令行工具

---
+++ src/cmd/lcd-st7735s/lcd_cmd.c
@@ -0,0 +1,89 @@
+/* --------------------------------------------------------------------------
+ *
+ * Commands for LCD ST7735S
+ * Copyright (C) 2018, Anthony Lee, All Rights Reserved
+ *
+ * This software is a freeware; it may be used and distributed according to
+ * the terms of The MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * File: lcd_cmd.c
+ * Description:
+ *
+ * --------------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include <string.h>
+
+extern int cmd_power(int argc, char **argv);
+extern int cmd_clear(int argc, char **argv);
+extern int cmd_show(int argc, char **argv);
+extern int cmd_update(int argc, char **argv);
+
+static char org_cmd[] = "lcd_cmd";
+
+static int show_usage(int argc, char **argv)
+{
+	printf("%s - Commands for LCD ST7735S\n\n", org_cmd);
+	printf("Usage: %s [commad] cmd_options\n\
+    Valid commands:\n\
+        power                   turn screen's power on/off\n\
+        clear                   clear screen\n\
+        show                    show characters on screen\n\
+        update                  whether to update screen when drawing\n", org_cmd);
+
+	return -1;
+}
+
+int main(int argc, char **argv)
+{
+	int (*func)(int, char**) = NULL;
+	int offset = 0;
+
+	char cmd[128];
+	char *str;
+
+	memset(cmd, 0, sizeof(cmd));
+
+	str = strrchr(argv[0], '/');
+	strncpy(cmd, (str == NULL ? argv[0] : str + 1), sizeof(cmd) - 1);
+
+	if (strcmp(cmd, org_cmd) == 0 && argc > 1) {
+		strncpy(cmd, "lcd_", 4);
+		strncpy(&cmd[4], argv[1], sizeof(cmd) - 5);
+		offset = 1;
+	}
+
+	if (strncmp(cmd, "lcd_", 4) == 0) {
+		if (strcmp(&cmd[4], "power") == 0)
+			func = cmd_power;
+		else if (strcmp(&cmd[4], "clear") == 0)
+			func = cmd_clear;
+		else if (strcmp(&cmd[4], "show") == 0)
+			func = cmd_show;
+		else if (strcmp(&cmd[4], "update") == 0)
+			func = cmd_update;
+	}
+
+	if (func == NULL)
+		func = show_usage;
+
+	return (*func)(argc - offset, argv + offset);
+}
+
---
+++ src/cmd/lcd-st7735s/lcd_update.c
@@ -0,0 +1,102 @@
+/* --------------------------------------------------------------------------
+ *
+ * Commands for LCD ST7735S
+ * Copyright (C) 2018, Anthony Lee, All Rights Reserved
+ *
+ * This software is a freeware; it may be used and distributed according to
+ * the terms of The MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * File: lcd_update.c
+ * Description:
+ *
+ * --------------------------------------------------------------------------*/
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+typedef unsigned char	bool;
+
+#ifndef true
+#define true 1
+#endif
+
+#ifndef false
+#define false 0
+#endif
+
+#include <lcd_st7735s_ioctl.h>
+
+#define DEFAULT_DEVICE		"/dev/spi-lcd0.1"
+
+static void show_usage(void)
+{
+	printf("lcd_update - Whether to update LCD when drawing\n\n");
+	printf("Usage: lcd_update [-D device] [state]\n\
+    device                path of device, default value is: %s\n\
+    state = 0,1           default value is: 1\n", DEFAULT_DEVICE);
+}
+
+#ifdef CMD_ALL_IN_ONE
+int cmd_update(int argc, char **argv)
+#else
+int main(int argc, char **argv)
+#endif
+{
+	int n, f, err = 0;
+	const char *dev_name = DEFAULT_DEVICE;
+	bool state;
+
+	for (n = 1; n < argc; n++) {
+		if (n < argc - 1 && strcmp(argv[n], "-D") == 0) {
+			dev_name = argv[++n];
+		} else {
+			break;
+		}
+	}
+	argc -= (--n);
+
+	if (argc > 2) {
+		show_usage();
+		exit(1);
+	}
+
+	if ((f = open(dev_name, O_RDWR)) < 0) {
+		perror("Open");
+		exit(1);
+	}
+
+	state = (argc == 1 || *(argv[n + 1]) == '1') ? true : false;
+
+	if ((err = ioctl(f, LCD_ST7735S_IOC_UPDATE, &state)) != 0)
+		perror("Ioctl");
+
+	close(f);
+
+	return err;
+}
+
