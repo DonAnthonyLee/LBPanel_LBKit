bd35b7ca12339264f7c92ae059c6ac24405341de
Anthony Lee <don.anthony.lee@gmail.com>
2019-01-12 01:10:38 +0800

    LBKit: NPI OLED Hat 改用 LBPanelCombiner 派生

---
+++ src/libs/lbk/add-ons/npi-oled-hat/NPIHatCombiner.h
@@ -0,0 +1,49 @@
+/* --------------------------------------------------------------------------
+ *
+ * Little Board Application Kit
+ * Copyright (C) 2018-2019, Anthony Lee, All Rights Reserved
+ *
+ * This software is a freeware; it may be used and distributed according to
+ * the terms of The MIT License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * File: NPIHatCombiner.h
+ * Description:
+ *
+ * --------------------------------------------------------------------------*/
+
+#ifndef __NPI_OLED_HAT_COMBINER_H__
+#define __NPI_OLED_HAT_COMBINER_H__
+
+#include <lbk/add-ons/LBPanelCombiner.h>
+
+#ifdef __cplusplus /* Just for C++ */
+
+class NPIHat : public LBPanelCombiner {
+public:
+	NPIHat();
+	virtual ~NPIHat();
+
+	virtual status_t	InitCheck(const char *options);
+};
+
+#endif /* __cplusplus */
+
+#endif /* __NPI_OLED_HAT_COMBINER_H__ */
+
